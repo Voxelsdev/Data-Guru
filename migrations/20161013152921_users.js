9097c3e52fc7444cdc61c459481c7df860ffd3a0
Tyler Miller <tylermillerhome@gmail.com>
2016-10-13 15:48:46 -0700

    Added Users / Projects migrations

--- migrations/20161013152921_users.js
+++ migrations/20161013152921_users.js
@@ -1,8 +1,14 @@
 'use strict';
-exports.up = function(knex, Promise) {
-
+exports.up = function(knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments();
+    table.string('email').notNullable().unique();
+    table.specificType('hashed_password', 'char(60)')
+         .notNullabe();
+    table.timestamps(true, true);
+  });
 };
 
-exports.down = function(knex, Promise) {
-
+exports.down = function(knex) {
+  return knex.schema.dropTable('users');
 };
--- migrations/20161013152950_projects.js
+++ migrations/20161013152950_projects.js
@@ -1,8 +1,19 @@
 'use strict';
-exports.up = function(knex, Promise) {
 
+exports.up = function(knex) {
+  return knex.schema.createTable('projects', (table) => {
+    table.increments();
+    table.integer('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE');
+    table.string('name', 30).notNullable().defaultTo('');
+    table.string('description', 500).notNullable().defaultTo('');
+    table.timestamps(true, true);
+  });
 };
 
-exports.down = function(knex, Promise) {
-
+exports.down = function(knex) {
+  return knex.schema.dropTable('projects');
 };
