0416eb65d0f50441754fe648b50a22b155b40419
Tyler Miller <tylermillerhome@gmail.com>
2016-10-15 15:07:36 -0700

    Finished project GET, doing POST

--- routes/projects.js
+++ routes/projects.js
@@ -21,14 +21,59 @@ function authorize (req, res, next) {
   });
 }
 
-router.post('/projects', authroize, ev(validations.post), (req, res, next) => {
-
-});
-
+// gets all of a users projects does not return datasets
 router.get('/projects', authorize, (req, res, next) => {
+  const userId = req.token;
 
+  if (userId) {
+    knex('projects')
+    .where('projects.user_id', userId)
+    .orderBy('projects.updated_at', 'DESC')
+    .then((rows) => {
+      res.send(camelizeKeys(rows))
+    })
+    .catch((err) => {
+      next(err);
+    });
+  }
 });
 
+// gets a specific user's project returns information of datasets
 router.get('/projects/:id', authorize, (req, res, next) => {
+  const userId = req.token;
 
+  if (userId) {
+    const projectId = req.params.id;
+    knex('projects')
+    .select()
+    .innerJoin('datasets_projects', 'datasets_projects.project_id', 'projects.id')
+    .innerJoin('datasets', 'datasets.id', 'datasets_projects.dataset_id')
+    .then((rows) => {
+      const project = camelizeKeys(rows);
+
+      res.send(project); // sends user's project and all the dataset info with it
+    })
+    .catch((err) => {
+      next(err);
+    });
+  }
+});
+
+// posts a new project for the user, needs work
+router.post('/projects', authorize, ev(validations.post), (req, res, next) => {
+  const { projectId } = req.body;
+  const { userId } = req.token;
+
+  if (userId) {
+    knex('projects')
+    .where('id', projectId)
+    .first()
+    .then((row) => {
+      if (!row) {
+        throw boom.create(404, 'Project not found');
+      }
+
+      // return knex('projects').insert(decamelizeKeys({ userId,  }))
+    })
+  }
 });
