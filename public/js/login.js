d7299c655cddc3a7264211701fca70d2e503f461
Tyler Miller <tylermillerhome@gmail.com>
2016-10-17 10:59:32 -0700

    Login page and js done

---
+++ public/js/login.js
@@ -0,0 +1,36 @@
+(() => {
+  'use strict';
+
+  $('.submit').on('click', () => {
+    const email = $('#account_circle').val();
+    const password = $('#verified_user').val();
+
+    if (!email) {
+      return Materialize.toast('Email must not be blank', 2000);
+    }
+
+    if (!password) {
+      return Materialize.toast('Password must not be blank', 2000);
+    }
+
+    if (password.length < 8) {
+      return Materialize.toast('Password must be at least 8 characers', 2000);
+    }
+
+    const options = {
+      contentType: 'application/json',
+      data: JSON.stringify({ email, password }),
+      dataType: 'json',
+      type: 'POST',
+      url: '/token'
+    };
+
+    $.ajax(options)
+    .done(() => {
+      window.location.href = '/profile.html';
+    })
+    .fail(($xhr) => {
+      Materialize.toast($xhr.responseText, 3000);
+    });
+  });
+})();
