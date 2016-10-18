0417f88c66c0ce3f95bdb21a433d3b8a1d112626
Tyler Miller <tylermillerhome@gmail.com>
2016-10-18 11:44:45 -0700

    projects are loaded in from the db when a user selects a project

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,9 +1,9 @@
 (() => {
-  function setview(view) {
+  function setview(view, projectId) {
     switch (view) {
       case 0:
         // view project
-        setProjectView();
+        setProjectView(projectId);
         break;
       case 1:
         // make dataset
@@ -14,12 +14,41 @@
     }
   }
 
-  function setProjectView() {
-    const $mainContain = $('<div class="row">');
-    const $subContain = $('<div class="col s8 offset-s2" id="sub-container">');
+  function setProjectView(projectId) {
+    $('#sub-container').empty();
+    const $mainUl = $('<ul class="collapsible" data-collapsible="accordion">');
 
+    const options = {
+      contentType: 'application/json',
+      type: 'GET',
+      url: `projects/${projectId}`
+    }
 
-    $('#info-container').empty();
+    $.ajax(options)
+    .done((projectInfo) => {
+      projectInfo = JSON.parse(projectInfo);
+      for (let i = 0; i < projectInfo.length) {
+        const $li = $('<li>');
+          const $header = $(`<div class="collapsible-header">${projectInfo[i].datasetName}`);
+          const $body = $('<div class="collapsible-body">');
+            const $checkBox = $(`<p class="p-checkbox"><input type="checkbox" class="filled-in custom-color" id="filled-in-box${i}" checked="checked"/><label for="filled-in-box${i}"></label></p>`)
+            const $email = $('<p class="d-email-me">Email me this dataset</p>');
+            const $desc = $(`<p class="d-description">${projectInfo[i].description}</p>`);
+            const $link = $(`<p class="d-link">${projectInfo[i].datasetLink}</p>`);
+
+        $body.append($link);
+        $body.append($desc);
+        $body.append($email);
+        $body.append($checkBox);
+        $li.append($body);
+        $li.append($header);
+        $mainUl.append($li);
+      }
+    })
+    .fail(($xhr) => {
+      Materialize.toast($xhr.responseText, 3000);
+    });
+
   }
 
   function setMakeDataset() {
@@ -83,6 +112,6 @@
   });
 
   $('.projects').on('click', (event) => {
-    setview(0);
+    setview(0, parseInt(event.target.siblings().text()));
   });
 })();
